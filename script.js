e88d5043dd40ac58137e1ec7bbd76d1df86872e3
mawilliams098 <mawilliams098@gmail.com>
2024-03-12 20:54:56 -0400

    Implement playRound logic

--- script.js
+++ script.js
@@ -2,30 +2,33 @@ function getRandomInt(max) {
     return Math.floor(Math.random() * max);
 }
 
-function mapIntToDraw(num) {
-    let draw;
-    if (num === 0) {
-        draw = "Rock";
-    } else if (num === 1) {
-        draw = "Paper";
-    } else {
-        draw = "Scissors";
-    }
-
-    return draw; 
-}
-
 function getComputerChoice() {
-    let num = getRandomInt(3);
-    let draw = mapIntToDraw(num);
-    console.log(draw);
+    let draw = getRandomInt(3);
     return draw;
 }
 
-// function playRound(playerSelection, computerSelection) {
-//     return null; 
-// }
+function getPlayerInput() {
+    return null;
+}
 
-// const playerSelection = "rock";
-// const computerSelection = getComputerChoice();
-// console.log(playRound(playerSelection, computerSelection))
+function playRound(playerSelection, computerSelection) {
+    console.log(playerSelection);
+    console.log(computerSelection);
+
+    if (playerSelection === computerSelection) {
+        return "It's a tie!"; 
+    } else if (playerSelection === 2 && computerSelection === 0) {
+        return "You win!";
+    } else if (computerSelection === 2 && playerSelection === 0) {
+        return "Computer wins!";
+    } else if (computerSelection < playerSelection) {
+        return "Computer wins!";
+    } else {
+        return "You win!";
+    }
+}
+
+const playerSelection = 0;
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));
+
