cfa5c522f0aa032fedb4a074724411cbc44e6ffb
mawilliams098 <mawilliams098@gmail.com>
2024-03-12 20:34:40 -0400

    Implement computer's random draw

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
-console.log("Hello world!");
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+}
+
+function mapIntToDraw(num) {
+    let draw;
+    if (num === 0) {
+        draw = "Rock";
+    } else if (num === 1) {
+        draw = "Paper";
+    } else {
+        draw = "Scissors";
+    }
+
+    return draw; 
+}
+
+function getComputerChoice() {
+    let num = getRandomInt(3);
+    let draw = mapIntToDraw(num);
+    console.log(draw);
+    return draw;
+}
+
+// function playRound(playerSelection, computerSelection) {
+//     return null; 
+// }
+
+// const playerSelection = "rock";
+// const computerSelection = getComputerChoice();
+// console.log(playRound(playerSelection, computerSelection))
