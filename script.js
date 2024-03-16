3e6ef2f3bceb34c806af1db66f00a996485a8a09
mawilliams098 <mawilliams098@gmail.com>
2024-03-16 16:30:02 -0400

    Alert when somebody reaches 5 wins

--- script.js
+++ script.js
@@ -38,83 +38,64 @@ let computerWins = 0;
 let roundCount = 0;
 
 const buttons = document.querySelectorAll("button");
-const results = document.createElement("div");
+const content = document.createElement("div");
+const tally = document.createElement("h4");
 const winnerList = document.createElement("ul");
-document.body.appendChild(results); 
-results.appendChild(winnerList);
+content.insertAdjacentElement('afterbegin', tally);
+document.body.appendChild(content); 
+content.appendChild(winnerList);
 
 
+
+function updateDOM(resultString) {
+    const roundWinner = document.createElement("li");
+    roundWinner.textContent = resultString;
+    tally.textContent = `Round ${roundCount} Results - Player: ${playerWins} Computer: ${computerWins}`;
+    winnerList.appendChild(roundWinner);
+}
 
 function playRound(playerSelection, computerSelection) {
-    const roundWinner = document.createElement("li");
+    let resultString;
+    roundCount++
     if (playerSelection === computerSelection) {
-        roundWinner.textContent = `It's a tie between ${translate(playerSelection)}!`;
-        winnerList.appendChild(roundWinner);
+        resultString = `It's a tie between ${translate(playerSelection)}!`;
+        updateDOM(resultString);
         return "tie"; 
     } else if (playerSelection === 2 && computerSelection === 0) {
-        roundWinner.textContent = "You win! Scissors beats Paper";
-        winnerList.appendChild(roundWinner);
-        return "player";
+        ++playerWins;
+        resultString = "You win! Scissors beats Paper";
+        updateDOM(resultString);
     } else if (computerSelection === 2 && playerSelection === 0) {
-        roundWinner.textContent = "You lose! Scissors beats paper";
-        winnerList.appendChild(roundWinner);
+        ++computerWins;
+        resultString = "You lose! Scissors beats paper";
+        updateDOM(resultString);
         return "computer";
     } else if (computerSelection < playerSelection) {
-        roundWinner.textContent = `You lose! ${translate(computerSelection)} beats ${translate(playerSelection)}`;
-        winnerList.appendChild(roundWinner);
+        ++computerWins;
+        resultString = `You lose! ${translate(computerSelection)} beats ${translate(playerSelection)}`;
+        updateDOM(resultString);
         return "computer";
     } else {
-        roundWinner.textContent = `You win! ${translate(playerSelection)} beats ${translate(computerSelection)}`;
-        winnerList.appendChild(roundWinner);
+        ++playerWins;
+        resultString = `You win! ${translate(playerSelection)} beats ${translate(computerSelection)}`;
+        updateDOM(resultString);
         return "player"
-    }
-   
+    } 
+
+
 }
 
 buttons.forEach((button) => {
     button.addEventListener("click", () => {
         const playerSelection = getPlayerInput(button.id);
         const computerSelection = getComputerChoice();
-        let winner = playRound(playerSelection, computerSelection);
-        ++roundCount;
-
-        if (winner == "player") {
-            ++playerWins;
-
-        } else if (winner == "computer") {
-            ++computerWins;
-        } else {
-            // announce that it was a tie 
-        }
-
-        if (playerWins === 5 || computerWins == 5) {
-            // announce the overall winner
-        }
+        playRound(playerSelection, computerSelection);
+        if (playerWins === 5) {
+            console.log('here!');
+            alert("You won overall!");
+        } else if (computerWins === 5) {
+            console.log('here');
+            alert("Computer won the most games!");
+        } 
     })
 })
-
-
-function playGame() {
-    let playerWins = 0;
-    let computerWins = 0;
-
-    for (let i = 0; i < 5; i++) {
-        const playerSelection = getPlayerInput();
-        const computerSelection = getComputerChoice();
-        let winner = playRound(playerSelection, computerSelection);
-        if (winner == "player") {
-            ++playerWins;
-        } else if (winner == "computer") {
-            ++computerWins;
-        }
-    }
-
-    // Report results 
-    if (playerWins > computerWins) {
-        console.log("You won the most games!")
-    } else if (computerWins > playerWins) {
-        console.log("Computer won the most games!")
-    } else {
-        console.log("It's an overall tie!")
-    }
-}
